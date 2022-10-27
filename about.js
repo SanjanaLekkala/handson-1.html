c720e26faf45f654a506d36e317e4fee08521545
SanjanaLekkala <94951069+SanjanaLekkala@users.noreply.github.com>
2022-10-27 17:58:15 +0530

    Add files via upload

--- about.js
+++ about.js
@@ -1,26 +1,23 @@
+import { Stack, Tab } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
-import { Typography } from "@mui/material";
-import { Stack } from "@mui/material";
+import Footer from "../Footer/footer";
+import Header from "../Header/header";
+import Content from "./content";
+
 const About = () => {
   return (
-    <Stack spacing={4}>
-      <Box>
-        <Typography variant="h6">ABOUT</Typography>
-        <Typography variant="h6">(adalimumab)</Typography>
-      </Box>
-      <Typography variant="h6" sx={{ width: { lg: "50%", xs: "auto" } }}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book
-      </Typography>
-      <Typography variant="h6" sx={{ width: { lg: "50%", xs: "auto" } }}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s.
-      </Typography>
-    </Stack>
+    <Box sx={{ color: "#F2F3F4" }}>
+      <Stack
+        sx={{ margin: { lg: 3, xs: 2 }, color: "#F2F3F4" }}
+        direction="column"
+        spacing={13}
+      >
+        <Header />
+        <Content />
+        <Footer />
+      </Stack>
+    </Box>
   );
 };
 
